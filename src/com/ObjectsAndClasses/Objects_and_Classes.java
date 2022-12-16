0df5f8b29b898722ea9d2edd142477cf6868fcfc
sheldon <liu_2648349896@126.com>
2022-12-16 14:42:40 +0800

    Objects_and_Classes

--- src/com/ObjectsAndClasses/Objects_and_Classes.java
+++ src/com/ObjectsAndClasses/Objects_and_Classes.java
@@ -10,6 +10,25 @@ import java.util.Date;
 public class Objects_and_Classes {
     public static void main(String[] args) {
 
-        System.out.println(new Date());
+        People[] people = new People[3];
+
+        people[0] = new People(1,"jake",200);
+        people[1] = new People(2,"tom",300);
+        people[2] = new People(3,"smith",400);
+
+        for (People people1 :people) {
+            System.out.println(people1);
+        }
+
+        //对象引用的是在堆中的地址
+        people[1] = people[0];
+        System.out.println(people[0].hashCode());
+        System.out.println(people[1].hashCode());
+
+        int[] resp = math.resp(3, 4);
+        for (int i :resp) {
+            System.out.println(i);
+        }
+
     }
 }
---
+++ src/com/ObjectsAndClasses/People.java
@@ -0,0 +1,50 @@
+package com.ObjectsAndClasses;
+
+/**
+ * @author sheldon
+ * @date 2022/12/16-14:11
+ */
+public class People {
+    private int id;
+    private String name;
+    private double price;
+
+    public People(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
